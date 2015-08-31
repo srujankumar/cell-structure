0a93acadc5595477e1c649334ab061af31f6a73d
Abdulsattar Mohammed <asattar.md@gmail.com>
2015-08-31 15:05:32 +0530

    Update to latest brand and chipper

--- js/cell-structure-config.js
+++ js/cell-structure-config.js
@@ -19,11 +19,12 @@ require.config( {
 
     // PhET plugins
     image: '../../chipper/js/requirejs-plugins/image',
+    mipmap: '../../chipper/js/requirejs-plugins/mipmap',
     string: '../../chipper/js/requirejs-plugins/string',
 
     // common directories, uppercase names to identify them in require imports
     AXON: '../../axon/js',
-    BRAND: '../../brand/js',
+    BRAND: '../../brand/' + phet.chipper.brand + '/js',
     DOT: '../../dot/js',
     JOIST: '../../joist/js',
     KITE: '../../kite/js',
